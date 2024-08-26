c994563f9828e9819d2025d03270873ecef386e2
Adam.Ozeri <Adam.Ozeri@s.afeka.ac.il>
2024-08-26 19:57:20 +0300

    changed library name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(project(":SwipeableCardStackLibrary"))
+    implementation(project(":LayoutLibrary"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "SwipeLib"
 include(":app")
-include(":SwipeableCardStackLibrary")
+include(":LayoutLibrary")
